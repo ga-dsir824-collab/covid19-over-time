66ace1a716af9ac1c72dc3346623f4bcd66d4450
Vivian Nguyen <nguy.vivian.vi@gmail.com>
2020-10-28 19:49:27 -0400

    add heroku dependencies

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"nguy.vivian.vi@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
